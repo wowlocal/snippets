45c44c2422b8e3f61ab7c715bd00927504f4c2b7
Misha Kh <kh.misha.9@icloud.com>
2026-02-15 15:56:20 +0300

    Initial Commit

---
+++ snippets/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  snippets
+//
+//  Created by Misha on 2/15/26.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
