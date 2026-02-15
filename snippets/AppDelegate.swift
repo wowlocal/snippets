9210da39812567f3c1ba962617983e3ea3969685
Misha Kh <kh.misha.9@icloud.com>
2026-02-15 18:45:15 +0300

    don't close the app on window close

--- snippets/AppDelegate.swift
+++ snippets/AppDelegate.swift
@@ -9,11 +9,34 @@ final class AppDelegate: NSObject, NSApplicationDelegate {
         expansionEngine.startIfNeeded()
     }
 
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        false
+    }
+
+    func applicationShouldHandleReopen(_ sender: NSApplication, hasVisibleWindows flag: Bool) -> Bool {
+        if !flag {
+            showMainWindow()
+        }
+        return true
+    }
+
     func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
         true
     }
 
     @IBAction func newDocument(_ sender: Any?) {
         NotificationCenter.default.post(name: .snippetsCreateNew, object: nil)
+    }
+
+    private func showMainWindow() {
+        if let window = NSApp.windows.first(where: { $0.contentViewController is ViewController }) {
+            window.makeKeyAndOrderFront(nil)
+        } else {
+            let storyboard = NSStoryboard(name: "Main", bundle: nil)
+            if let wc = storyboard.instantiateInitialController() as? NSWindowController {
+                wc.showWindow(nil)
+            }
+        }
+        NSApp.activate(ignoringOtherApps: true)
     }
 }
